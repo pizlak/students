c4da0f21c921d6ba5f5dc90e9acb95940511f4bc
pizlac <baldesovlizka97@gmail.com>
2024-03-21 22:48:56 +0300

    21.03.2024 v2

--- public/studentTable.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*require_once '../config/config.php';
-$controller = new \app\Controller\StudentController();
-if (isset($_POST['search'])){
-    $controller->viewStudentsTable();
-} else {
-    $controller->viewSearchStudents();
-}*/
